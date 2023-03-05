e123629eee9139655e52b9552c55746857590c5f
Darin Hajou <it_related@hotmail.com>
2023-03-05 17:13:57 +0100

    Formatted the codebase

--- __app/js/modules/search.js
+++ __app/js/modules/search.js
@@ -1,59 +1,56 @@
-  import { fetchEvents, renderEvents, baseUrl, renderEventDetails } from './events.js';
-  import { clientID } from '../env.js';
+import { fetchEvents, renderEvents, baseUrl, renderEventDetails } from './events.js';
+import { clientID } from '../env.js';
 
+export default function searchEvent() {
+  const searchButton = document.getElementById('event-search__button');
+  const cityInput = document.getElementById('event-search__location');
+  const resultsContainer = document.getElementById('results-container');
 
-  export default function searchEvent() {
-    const searchButton = document.getElementById('event-search__button');
-    const cityInput = document.getElementById('event-search__location');
-    const resultsContainer = document.getElementById('results-container');
-  
-    if (searchButton && cityInput && resultsContainer) {
-      searchButton.addEventListener('click', async function () {
-        resultsContainer.innerHTML = 'Loading...';
-        const city = cityInput.value;
-  
-        try {
-          if (!city) {
-            throw new Error('Please enter a valid city name');
-          }
-  
-          const events = await fetchEvents(city);
-          if (events.length === 0) {
-            resultsContainer.innerHTML = 'No events found for this city.';
-            return;
-          }
-  
-          const resultsHTML = await renderEvents(events);
-          resultsContainer.innerHTML = resultsHTML;
-  
-          const eventButtons = document.querySelectorAll('.result-container__moreinfo-button');
-          eventButtons.forEach((button) => {
-            button.addEventListener('click', async (event) => { 
-              const eventId = event.target.dataset.id;
-              const eventDetails = await fetchEventDetails(eventId);
-              renderEventDetails(eventDetails);
-              window.location.href = `../event-details.html?id=${eventId}`;
-            });
-          });
-        } catch (error) {
-          resultsContainer.innerHTML =
-            `An error occurred: ${error.message}. Please try again later.`;
-          console.error(error);
+  if (searchButton && cityInput && resultsContainer) {
+    searchButton.addEventListener('click', async function () {
+      resultsContainer.innerHTML = 'Loading...';
+      const city = cityInput.value;
+
+      try {
+        if (!city) {
+          throw new Error('Please enter a valid city name');
         }
-      });
-    }
+
+        const events = await fetchEvents(city);
+        if (events.length === 0) {
+          resultsContainer.innerHTML = 'No events found for this city.';
+          return;
+        }
+
+        const resultsHTML = await renderEvents(events);
+        resultsContainer.innerHTML = resultsHTML;
+
+        const eventButtons = document.querySelectorAll('.result-container__moreinfo-button');
+        eventButtons.forEach((button) => {
+          button.addEventListener('click', async (event) => { 
+            const eventId = event.target.dataset.id;
+            const eventDetails = await fetchEventDetails(eventId);
+            renderEventDetails(eventDetails);
+            window.location.href = `../event-details.html?id=${eventId}`;
+          });
+        });
+      } catch (error) {
+        resultsContainer.innerHTML =
+          `An error occurred: ${error.message}. Please try again later.`;
+        console.error(error);
+      }
+    });
+  }
+}
+
+export async function fetchEventDetails(eventId) {
+  const endpoint = `${baseUrl}discovery/v2/events/${eventId}.json?apikey=${clientID}`;
+  const response = await fetch(endpoint);
+
+  if (!response.ok) {
+    throw new Error('Unable to fetch event details.');
   }
 
-
-  export async function fetchEventDetails(eventId) {
-    const endpoint = `${baseUrl}discovery/v2/events/${eventId}.json?apikey=${clientID}`;
-    const response = await fetch(endpoint);
-
-    if (!response.ok) {
-      throw new Error('Unable to fetch event details.');
-    }
-
-    const data = await response.json();
-    return data;
-  }
-
+  const data = await response.json();
+  return data;
+}
