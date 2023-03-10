9132d33b66789185ead8be435af5c850cdc8f296
Darin Hajou <it_related@hotmail.com>
2023-03-10 06:21:38 +0100

    Code formattign and file name changes

--- __app/js/modules/mapbox.js
+++ __app/js/modules/mapbox.js
@@ -1,16 +1,14 @@
- export default function renderMap(longitude, latitude) {
-    
-    mapboxgl.accessToken = 'pk.eyJ1IjoiY29kZXRlZ3JpdHkiLCJhIjoiY2xld2p0ZnBnMGhnbzNzbzRxaTltZHUwcyJ9.ztTEqEq4WeTRyV68oE3wMg';
-    const map = new mapboxgl.Map({  
-      container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v11',
-      center: [longitude, latitude],
-      zoom: 12
-    });
-    
-    const marker = new mapboxgl.Marker()
-      .setLngLat([longitude, latitude])
-      .addTo(map);
-  }
-   
-  
+export default function renderMap(longitude, latitude) {
+  mapboxgl.accessToken = 'pk.eyJ1IjoiY29kZXRlZ3JpdHkiLCJhIjoiY2xld2p0ZnBnMGhnbzNzbzRxaTltZHUwcyJ9.ztTEqEq4WeTRyV68oE3wMg';
+
+  const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [longitude, latitude],
+    zoom: 12
+  });
+
+  const marker = new mapboxgl.Marker()
+    .setLngLat([longitude, latitude])
+    .addTo(map);
+}
